55fe35491201cd3c8f10c9947d36ada5fa2a47bc
DevelopBirb <juanmajm360@gmail.com>
2024-03-30 15:22:09 +0100

    meter el mapa del plan maestro pa probar

--- juegojuego/Assets/0_Scripts/Coche/CarTurnAround.cs
+++ juegojuego/Assets/0_Scripts/Coche/CarTurnAround.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 public class CarTurnAround : MonoBehaviour
 {
-    private Rigidbody carRigidbody;
+    [SerializeField]private Rigidbody carRigidbody;
     private float turnAroundValue;
 
     [SerializeField] private float maxTurnAroundVelocity;
