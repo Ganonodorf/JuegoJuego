82f7ca03ce483c0661d7a313e03ce61d050586d6
Ganonodorf <juanjogarridobascaran@gmail.com>
2024-08-21 16:43:44 +0200

    El dialogo de los fabricantes ahora sale letra a letra para el player; el juego ahora comienza en el estado que se elija, y si es la intro, funciona bien sin necesidad de que haya el trigger; actualizados los nombres de los personajes de la segunda zona para que tengan color; actualizados los diálogos de la segunda zona para que vayan al hub

--- juegojuego/Assets/0_Scripts/Dialog/SkipTypewriterForPlayer.cs
+++ juegojuego/Assets/0_Scripts/Dialog/SkipTypewriterForPlayer.cs
@@ -1,10 +1,11 @@
 using UnityEngine;
 using PixelCrushers.DialogueSystem;
+using Unity.VisualScripting;
 public class SkipTypewriterForPlayer : MonoBehaviour
 {
     void OnConversationLine(Subtitle subtitle)
     {
-        if (subtitle.speakerInfo.isPlayer)
+        if (subtitle.speakerInfo.isPlayer && subtitle.listenerInfo.Name != "Manu" && subtitle.listenerInfo.Name != "Paco")
         {
             subtitle.formattedText.text = @"\^" + subtitle.formattedText.text;
         }
