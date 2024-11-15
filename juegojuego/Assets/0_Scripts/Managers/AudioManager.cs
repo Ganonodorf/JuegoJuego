ba34c1fab83193c2ee4cdb80fbb998d8a80a01e9
Ganonodorf <juanjogarridobascaran@gmail.com>
2024-11-15 15:57:52 +0100

    Metidos sonidos de abrir y cerrar inventario

--- juegojuego/Assets/0_Scripts/Managers/AudioManager.cs
+++ juegojuego/Assets/0_Scripts/Managers/AudioManager.cs
@@ -4,15 +4,35 @@ public class AudioManager : MonoBehaviour
 {
     public static AudioManager Instance;
 
+    [SerializeField] private AudioClip cocheAbriendose;
+    [SerializeField] private AudioClip cocheCerrandose;
+
+    private AudioSource audioSource;
+
     private void Awake()
     {
         HacerloInmortal();
+
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameManager.OnGameStateChanged += CambioEstadoJuego;
+    }
+
+    private void CambioEstadoJuego(GameState nuevoEstadoJuego, GameState viejoEstadoJuego)
+    {
+        if(nuevoEstadoJuego == GameState.Inventario)
+        {
+            audioSource.PlayOneShot(cocheAbriendose);
+        }
+
+        if(viejoEstadoJuego == GameState.Inventario)
+        {
+            audioSource.PlayOneShot(cocheCerrandose);
+        }
     }
 
     private void HacerloInmortal()
