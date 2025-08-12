0ecad182a221bf30f00e78cf6530dfcdc6d16cfb
Ganonodorf <juanjogarridobascaran@gmail.com>
2025-08-12 12:07:35 +0200

    feat: add ending animation inaction

--- juegojuego/Assets/0_Scripts/Coche_AnimacionFinal/Coche_AnimacionFinal.cs
+++ juegojuego/Assets/0_Scripts/Coche_AnimacionFinal/Coche_AnimacionFinal.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using PixelCrushers.DialogueSystem;
 using UnityEngine;
 
 public class Coche_AnimacionFinal : MonoBehaviour
@@ -9,11 +10,25 @@ public class Coche_AnimacionFinal : MonoBehaviour
     
     [SerializeField]
     private GameObject animationCamera;
+    
+    public void EndFinalConversation()
+    {
+        for (int i = DialogueManager.instance.activeConversations.Count - 1; i >= 0; i--)
+        {
+            DialogueManager.instance.activeConversations[i].conversationController.Close();
+        }
+    }
+    
+    public void AnimationStarted()
+    {
+        GameManager.Instance.UpdateGameState(GameState.Dialogo);
+        teleportTriggerZone.TeleportPlayerToSpawnPoint();
+    }
 
     public void AnimationEnded()
     {
-        teleportTriggerZone.TeleportPlayerToSpawnPoint();
         animationCamera.SetActive(false);
         
+        GameManager.Instance.UpdateGameState(GameState.Conduciendo);
     }
 }
