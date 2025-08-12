254c72073f8bf63534ae279974b62dde50a39c00
Ganonodorf <juanjogarridobascaran@gmail.com>
2025-08-12 19:28:23 +0200

    feat: add two endings to the game

--- juegojuego/Assets/0_Scripts/FinJuego/FinJuego.cs
+++ juegojuego/Assets/0_Scripts/FinJuego/FinJuego.cs
@@ -1,12 +1,32 @@
+using PixelCrushers.DialogueSystem;
 using UnityEngine;
 
 public class FinJuego : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject PlayerGO;
+    
+    [SerializeField]
+    private GameObject MagdalenitaGO;
+    
     private void OnTriggerEnter(Collider collider)
     {
         if(collider.transform.name == Constantes.Player.NOMBRE_GO)
         {
+            PlayerGO.SetActive(false);
+            MagdalenitaGO.transform.Rotate(Vector3.up, 180f);
             GameManager.Instance.UpdateGameState(GameState.SecuenciaFinal);
         }
+    }
+
+    public void FinVolverALaMugre()
+    {
+        for (int i = DialogueManager.instance.activeConversations.Count - 1; i >= 0; i--)
+        {
+            DialogueManager.instance.activeConversations[i].conversationController.Close();
+        }
+        
+        PlayerGO.SetActive(false);
+        GameManager.Instance.UpdateGameState(GameState.SecuenciaFinal);
     }
 }
